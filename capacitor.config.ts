f1acc922b7dc3a22b5f388bacb60b43de02aa015
hugogaytan10 <estardus9@gmail.com>
2023-05-20 11:01:13 -0600

    mi app ionic

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'Photo Galery',
+  appName: 'photo-galery',
   webDir: 'build',
   server: {
     androidScheme: 'https'
